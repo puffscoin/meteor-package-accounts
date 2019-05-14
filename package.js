88fd110a9b3a58282a04ffa468cfdad438ebdf6d
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-05-14 16:06:15 -0300

    Update package.js

--- package.js
+++ package.js
@@ -1,9 +1,9 @@
 Package.describe({
-  name: "ethereum:accounts",
+  name: "puffscoin:accounts",
   summary:
-    "Provides and updates the ethereum accounts in the Accounts collection",
+    "Provides and updates PUFFScoin accounts in the Accounts collection",
   version: "1.1.0",
-  git: "http://github.com/ethereum/meteor-package-accounts"
+  git: "http://github.com/puffscoin/meteor-package-accounts"
 });
 
 Package.onUse(function(api) {
@@ -12,15 +12,15 @@ Package.onUse(function(api) {
   api.use("mongo", ["client", "server"]);
 
   api.use("frozeman:persistent-minimongo@0.1.8", "client");
-  api.use("ethereum:web3@1.0.0-beta.33", ["client", "server"]);
+  api.use("puffscoin:web3@1.0.0-beta.33", ["client", "server"]);
 
-  api.export(["EthAccounts"], ["client", "server"]);
+  api.export(["PuffsAccounts"], ["client", "server"]);
 
   api.addFiles("accounts.js", ["client", "server"]);
 });
 
 // Package.onTest(function(api) {
 //   api.use('tinytest');
-//   api.use('ethereum:accounts');
+//   api.use('puffscoin:accounts');
 //   api.addFiles('accounts-tests.js');
 // });
